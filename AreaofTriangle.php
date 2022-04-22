ec9e282291efdf0435c58cf8b5f0c646fbd4fbc5
gabmor38 <Gabriela.morenor@gmail.com>
2022-04-22 11:41:14 -0400

    function oif area of a triangle

---
+++ AreaofTriangle.php
@@ -0,0 +1,11 @@
+<?php
+
+/*  Area of Triangle
+Write a function that takes the base and height of a triangle and return its area.
+*/ 
+
+function triArea($base, $height) {
+  return ($base * $height) / 2;
+}
+
+?>
